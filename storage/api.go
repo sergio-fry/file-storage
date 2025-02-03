3c57dac5549519d778ab44d709f8f7058933ca32
Sergei O. Udalov <udalov.x@mail.ru>
2025-02-03 20:51:40 +0300

    upload works, catn compile

--- storage/api.go
+++ storage/api.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"path/filepath"
 )
 
-var storage Storage = Storage{path: "/Users/sergei/code/sergio-fry/file-storage/data"}
+var globalStorage = Storage{path: "/Users/sergei/code/sergio-fry/file-storage/data"}
 
 func getFile(w http.ResponseWriter, r *http.Request) {
-	var result = storage.read(r.URL.Query().Get("name"))
+	var result = globalStorage.read(r.URL.Query().Get("name"))
 	defer result.Close()
 	io.Copy(w, result)
 }
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error retrieving the file: %v", err), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	filename := filepath.Base(header.Filename)
+
+	globalStorage.write(filename, file)
+
+}
+
 func main() {
 	http.HandleFunc("/files", getFile)
+	http.HandleFunc("/upload", uploadFile)
 
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- storage/api_test.go
+++ storage/api_test.go
@@ -1,14 +1,18 @@
 package storage
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
-	storage.write("foo.txt", strings.NewReader("bar"))
+func TestGetFiles(t *testing.T) {
+	globalStorage.write("foo.txt", strings.NewReader("bar"))
 
 	req, err := http.NewRequest("GET", "/files?name=foo.txt", nil)
 	if err != nil {
@@ -25,5 +29,43 @@ func TestHandler(t *testing.T) {
 	expected := "bar"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+	var file = strings.NewReader("bar")
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", "foo.txt")
+	if err != nil {
+		fmt.Println("Error creating form file:", err)
+		return
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		fmt.Println("Error copying file:", err)
+		return
+	}
+	err = writer.Close()
+	if err != nil {
+		fmt.Println("Error closing writer:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "/upload", body)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+	uploadFile(rr, req)
+
+	var result []byte
+	result, err = io.ReadAll(globalStorage.read("foo.txt"))
+
+	if string(result) != "bar" {
+		t.Errorf("handler returned unexpected body: got %v want %v", string(result), "bar")
 	}
 }
